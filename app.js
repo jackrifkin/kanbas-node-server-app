c3f60a187f6920e5174d5eed00c19de7f4f9ad79
Jack Rifkin <jack.rifkin@icloud.com>
2023-11-16 21:24:06 -0500

    lab5

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const express = require('express')
-const app = express()
-app.get('/hello', (req, res) => {res.send('Hello World!')})
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './Lab5.js';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+Lab5(app);
 app.listen(4000)
